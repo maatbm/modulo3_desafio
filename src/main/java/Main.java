7825d8c99455576b9141d411c30736313d17e020
maatbm <matheusbmoraes0@gmail.com>
2025-03-20 09:28:50 -0300

    Menu principial com verificação das opções do usuário

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int homeOpt;
 
+        RulesAndOptions rules_options = new RulesAndOptions();
+        while (true){
+            try(Scanner sc = new Scanner(System.in)){ // Recebe a oção do usuário e faz as verficações de erro necessárias.
+                while (true){
+                    try{
+                        rules_options.homeOptions();
+                        homeOpt = sc.nextInt();
+                        if(homeOpt<=0 || homeOpt>4){
+                            System.out.println("Oopção inválida inserida");
+                        }else if (homeOpt == 4){
+                            System.out.println("Jogo encerrado com sucesso!");
+                            System.exit(0);
+                        } else {
+                            break;
+                        }
+                    } catch (Exception e){
+                        System.out.println("Insira apenas números:");
+                        sc.nextLine();
+                    }
+                }
+            }
+            System.out.println("funcionou");
+            break;
+        }
     }
 }
---
+++ src/main/java/RulesAndOptions.java
@@ -0,0 +1,10 @@
+public class RulesAndOptions {
+    public void homeOptions(){
+        System.out.println("Bem-vindo ao jogo de adivinhação!");
+        System.out.println("1 - Iniciar novo jogo;");
+        System.out.println("2 - Ver regras;");
+        System.out.println("3 - Ver histórico de pontuações;");
+        System.out.println("4 - Sair");
+        System.out.println("Insira o número da opção desejada:");
+    }
+}
